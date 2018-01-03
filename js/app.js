1ff41b67f25b121e3e0a1a3b4848cb7a22a04fd4
Jeremy <jpjazzy@hotmail.com>
2018-01-02 18:44:18 -0800

    further styling on homepage with slider

--- js/app.js
+++ js/app.js
@@ -1,7 +1,34 @@
 'use strict';
 
-$(document).ready(function(){
-  $('.game-slider').slick({
-    setting-name: setting-value
-  });
-});
+/************* SLIDER JS ************************/
+var slideIndex = 1;
+showSlides(slideIndex);
+
+var transitionSpeed = 8000;
+
+var autoplay = setInterval(function() {plusSlides(1);}, transitionSpeed);
+
+function userClickPlusSlides(n) {
+  clearInterval(autoplay);
+  plusSlides(n);
+  autoplay = setInterval(function() {plusSlides(1);}, transitionSpeed);
+}
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName('mySlides');
+  if (n > slides.length) {slideIndex = 1;}
+  if (n < 1) {slideIndex = slides.length;}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = 'none';
+  }
+  slides[slideIndex - 1].style.display = 'block';
+}
